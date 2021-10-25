482f4933e7998fc39538aad9686ca1e8f76c6832
Noah Altunian <nbaltunian@gmail.com>
2021-10-25 02:52:57 -0400

    Apply formatting to time.ts

--- bin/time.ts
+++ bin/time.ts
@@ -1,7 +1,7 @@
-const args = Deno.args.flat()
+const args = Deno.args.flat();
 if (args.length < 1) {
-  console.error("Usage: time command [arguments...]")
-  Deno.exit(1)
+  console.error("Usage: time command [arguments...]");
+  Deno.exit(1);
 }
 
 const p = Deno.run({
@@ -13,15 +13,15 @@ const [status, stdout, stderr] = await Promise.all([
   p.status(),
   p.output(),
   p.stderrOutput(),
-])
+]);
 p.close();
 
 if (status.code === 0) {
-  await Deno.stdout.write(stdout)
+  await Deno.stdout.write(stdout);
 } else {
-  const err = new TextDecoder().decode(stderr)
-  console.log(err)
-  Deno.exit(2)
+  const err = new TextDecoder().decode(stderr);
+  console.log(err);
+  Deno.exit(2);
 }
 
 console.log(String(`\nreal    ${performance.now()}`));
