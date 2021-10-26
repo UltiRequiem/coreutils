aaded323fd60ccdd45476c1749422b0bc1a4bd05
Noah Altunian <nbaltunian@gmail.com>
2021-10-25 23:21:51 -0400

    Refactor

--- bin/echo.ts
+++ bin/echo.ts
@@ -1,5 +1,4 @@
-if (Deno.args.length === 1 && typeof Deno.args[0] === "string") {
+if (Deno.args.length === 1 && typeof Deno.args[0] === "string")
   console.log(Deno.args[0]);
-  Deno.exit(0);
-}
-console.log(Deno.args.join(" "));
+else
+  console.log(Deno.args.join(" "));
