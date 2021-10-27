b2818a3cfcbd380655a0e00a1da261602a26b43f
Noah Altunian <nbaltunian@gmail.com>
2021-10-26 20:43:11 -0400

    Parse 1..n envars and print

--- bin/printenv.ts
+++ bin/printenv.ts
@@ -1,5 +1,11 @@
-for (const envar in Deno.env.toObject()) {
-  if (envar !== "_") {
+if (Deno.args.length > 0) {
+  for (const envar of Deno.args.flat()) {
     console.log(`${envar}=${Deno.env.get(envar)}`);
+  }
+} else {
+  for (const envar in Deno.env.toObject()) {
+    if (envar !== "_") {
+      console.log(`${envar}=${Deno.env.get(envar)}`);
+    }
   }
 }
