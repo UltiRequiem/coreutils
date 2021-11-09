c519624254af7b169d705706286698e9cd87b0d4
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-09 09:34:43 -0500

    :recycle: Update

--- bin/cat.ts
+++ bin/cat.ts
@@ -1,5 +1,11 @@
 import { writeAll } from "../deps.ts";
 
+if (Deno.args.length === 0) {
+  while (true) {
+    console.log(` ${prompt("")}`);
+  }
+}
+
 for (const file of Deno.args) {
   writeAll(Deno.stdout, await Deno.readFile(file));
 }
