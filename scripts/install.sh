7b8c746eb244c20572d9bf453342cfd6eb3ccc7e
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-10 13:38:53 -0500

    :bug: Fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,8 @@ if ! command -v deno &>/dev/null; then
 	echo "Deno is not in PATH." && exit
 fi
 
-for bin in "commands/*"; do
-  echo $bin
-	# echo "deno install -f -A -n js$bin https://deno.land/x/coreutils/command/$bin/$bin.ts" 
+commands_to_install=("yes" "pwd" "whoami" "clear" "cat" "echo" "time" "printenv" "arch" "touch" "ls")
+
+for bin in "${commands_to_install[@]}"; do
+	echo "deno install -f -A -n js$bin https://deno.land/x/coreutils/command/$bin/$bin.ts" | sh
 done
