71343846d8678a36cb7df8a3e884209036eefd1e
GitHub <noreply@github.com>
2022-07-13 00:11:36 +0000

    :arrow_up: Update Dependencies

--- deps_dev.ts
+++ deps_dev.ts
@@ -1,1 +1,1 @@
-export { assert } from "https://deno.land/std@0.147.0/testing/asserts.ts";
+export { assert } from "https://deno.land/std@0.148.0/testing/asserts.ts";
--- deps_std.ts
+++ deps_std.ts
@@ -1,2 +1,2 @@
-export { writeAll } from "https://deno.land/std@0.147.0/streams/conversion.ts";
-export { ensureFile } from "https://deno.land/std@0.147.0/fs/mod.ts";
+export { writeAll } from "https://deno.land/std@0.148.0/streams/conversion.ts";
+export { ensureFile } from "https://deno.land/std@0.148.0/fs/mod.ts";
