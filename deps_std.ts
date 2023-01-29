2788988c11a0edb9562507ce0b9f49c865ab56c9
GitHub <noreply@github.com>
2023-01-29 00:10:40 +0000

    :arrow_up: Update Dependencies

--- deps_std.ts
+++ deps_std.ts
@@ -1,2 +1,2 @@
-export { writeAll } from "https://deno.land/std@0.174.0/streams/conversion.ts";
-export { ensureFile } from "https://deno.land/std@0.174.0/fs/mod.ts";
+export { writeAll } from "https://deno.land/std@0.175.0/streams/conversion.ts";
+export { ensureFile } from "https://deno.land/std@0.175.0/fs/mod.ts";
